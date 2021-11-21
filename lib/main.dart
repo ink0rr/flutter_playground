52eae12373538f15979157b777bf5f1b8207d847
ink0rr <tomflynnbeltsazar@gmail.com>
2021-11-21 22:31:14 +0700

    Change app primary color and text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
+        textTheme: GoogleFonts.notoSansTextTheme()
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
