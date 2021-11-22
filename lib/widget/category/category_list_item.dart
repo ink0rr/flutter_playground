12b0fa0b3b3abc99e540d6361bad8283d73fe678
ink0rr <tomflynnbeltsazar@gmail.com>
2021-11-22 15:52:18 +0700

    Add category widget

---
+++ lib/widget/category/category_list_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CategoryListItemWidget extends StatelessWidget {
+  const CategoryListItemWidget({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          const CircleAvatar(
+            radius: 24,
+            backgroundImage: NetworkImage(
+              'https://i.pinimg.com/originals/00/0a/99/000a99df942d55d5be69a7d5c1a43319.jpg',
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(title)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/category/category_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'category_list_item.dart';
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Categories",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            TextButton(
+              onPressed: () => {
+                //do something
+              },
+              child: const Text(
+                "See all",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 96,
+          child: ListView.builder(
+            itemCount: 8,
+            shrinkWrap: false,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              return CategoryListItemWidget(title: 'Category ${index + 1}');
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
